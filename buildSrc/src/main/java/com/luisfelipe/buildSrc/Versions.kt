e4de84d2cbf7723aa94591716b704dc68d1f26a6
lf <luisfelipecf94@gmail.com>
2021-01-08 11:46:04 -0300

    Organizando versões de dependência

--- buildSrc/src/main/java/com/luisfelipe/buildSrc/Versions.kt
+++ buildSrc/src/main/java/com/luisfelipe/buildSrc/Versions.kt
@@ -2,6 +2,7 @@ package com.luisfelipe.buildSrc
 
 object Versions {
 
+    // Gradle
     const val compile_sdk = 30
     const val build_tools = "30.0.1"
     const val min_sdk = 21
@@ -9,34 +10,42 @@ object Versions {
     const val version_code = 1
     const val version_name = "1.0"
 
+    // DI
     const val daggerHilt = "2.28-alpha"
+    const val daggerHiltCompiler = "1.0.0-alpha02"
 
+    // Core
+    const val kotlin = "1.4.21"
+    const val core = "1.3.2"
+    const val legacySupport = "1.0.0"
+    const val coroutines = "1.3.7"
+    const val coroutinesAndroid = "1.4.1"
+
+    // Lifecyle-Aware Components
     const val lifecycle = "2.2.0"
     const val navigation = "2.3.2"
     const val liveData = "2.2.0"
     const val viewModel = "2.2.0"
+    const val hiltLifecycleViewModel = "1.0.0-alpha02"
+
+    // UI
     const val material_version = "1.2.1"
     const val constraint_layout = "2.0.4"
-    const val core = "1.3.2"
-    const val legacySupport = "1.0.0"
-    const val kotlin = "1.4.21"
-    const val coroutines = "1.3.7"
-    const val coroutinesAndroid = "1.4.1"
     const val lottieAnimation = "3.6.0"
+    const val app_compat = "1.2.0"
+    const val fragment_ktx = "1.2.5"
+    const val activityKtx = "1.1.0"
+    const val picasso = "2.71828"
+    const val roundedImageView = "2.3.0"
+    const val recyclerViewSwipeDecorator = "1.2.3"
+
+    // Data
     const val room = "2.2.6"
     const val gson = "2.8.6"
+
+    // Testing
     const val mockk = "1.10.0"
     const val junit = "4.13.1"
     const val junitAndroid = "1.1.2"
     const val espresso = "3.3.0"
-    const val app_compat = "1.2.0"
-    const val fragment_ktx = "1.2.5"
-    const val activityKtx = "1.1.0"
-    const val hiltLifecycleViewModel = "1.0.0-alpha02"
-    const val daggerHiltCompiler = "1.0.0-alpha02"
-    const val recyclerViewSwipeDecorator = "1.2.3"
-
-    const val picasso = "2.71828"
-
-    const val roundedImageView = "2.3.0"
 }
